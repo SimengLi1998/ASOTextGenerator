c7cb908ab36748cc29570caf29f2450fc50530e0
Simeng Li <87021559+SimengLi1998@users.noreply.github.com>
2023-12-06 18:23:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from routers import service
+from fastapi import FastAPI
+import uvicorn
+import sys
+import os
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+app = FastAPI()
+app.include_router(service.serviceRouter,prefix='/service',tags=["service模块"])
+
+
+if __name__  == '__main__':
+    if sys.argv[1]:
+        os.environ['dev_mode'] = sys.argv[1]
+    else:
+        os.environ['dev_mode'] = 'dev'
+    logging.info(f"dev_mode: {sys.argv[1]}")
+    uvicorn.run('main:app',host='0.0.0.0',port=7800,reload=True, workers=4)
